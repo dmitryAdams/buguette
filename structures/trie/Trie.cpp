5fa76de242f7ef4d530efff6b0da347eec57563a
adams <dvalekseev2007@gmail.com>
2024-10-24 11:40:14 +0300

    Написанн бор для хранения зарезервированных слов

---
+++ structures/trie/Trie.cpp
@@ -0,0 +1,58 @@
+//
+// Created by adams on 10/24/24.
+//
+
+#include "Trie.h"
+
+Trie::Trie(const std::vector<std::string>& strs)
+{
+    for (const auto& s : strs)
+    {
+        addString(s);
+    }
+}
+
+void Trie::addString(const std::string& s)
+{
+    auto v = root_;
+    for (auto c : s)
+    {
+        if (!v->children_.count(c))
+        {
+            v->children_[c] = new TrieNode;
+        }
+        v = v->children_[c];
+    }
+    v->is_terminate_ = true;
+}
+
+bool Trie::checkString(const std::string& s)
+{
+    auto v = root_;
+    for (auto c : s)
+    {
+        if (!v->children_.count(c))
+        {
+            return false;
+        }
+        v = v->children_[c];
+    }
+    return v->is_terminate_;
+}
+
+Trie::~Trie()
+{
+    TrieNode::kill(root_);
+}
+
+void Trie::TrieNode::kill(TrieNode* v)
+{
+    if (v)
+    {
+        for (auto [c, i] : v->children_)
+        {
+            kill(i);
+        }
+    }
+    delete v;
+}
